8e46887ba32093418117c67fcf60d3f95f053ca5
asierbayon <bayonasier@gmail.com>
2021-01-23 17:14:56 +0100

    feat: partials registered

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('./config/hbs.config');
 
+/* Middlewares */
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* View setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+/* Routes */
 const router = require('./config/routes.config');
 app.use('/', router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.info(`🏔️ PeakHunt listens at port ${port}`);
+	console.info(`PeakHunt listens at port ${port}`);
 });
