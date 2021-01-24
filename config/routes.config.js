621171facf282c1a1c9551547f17c0fc2a4fbb26
asierbayon <bayonasier@gmail.com>
2021-01-24 19:46:06 +0100

    feat: /routes basic logic developed

--- config/routes.config.js
+++ config/routes.config.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const commonController = require('../controllers/common.controller');
+const routesController = require('../controllers/routes.controller');
 
 router.get('/', commonController.home);
+router.get('/routes', routesController.list)
 
 module.exports = router;
