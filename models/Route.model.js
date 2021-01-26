05103557194d792a766c71641b6f1bddbe818320
asierbayon <bayonasier@gmail.com>
2021-01-26 15:45:38 +0100

    feat: image by url added to Route Schema

--- models/Route.model.js
+++ models/Route.model.js
@@ -29,6 +29,10 @@ const routeSchema = new Schema({
         type: String,
         required: 'Please enter a description for this route.'
     },
+    image: {
+        type: String,
+        default: 'https://images.unsplash.com/photo-1520962880247-cfaf541c8724?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1778&q=80'
+    },
     video: {
         type: String
     }
