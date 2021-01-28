ecaab96c12e5c8ec1591c0b885338f37f80d4e78
asierbayon <bayonasier@gmail.com>
2021-01-28 14:33:11 +0100

    feat: list search form dropdowns hbs config

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -2,3 +2,11 @@ const hbs = require('hbs');
 const path = require('path');
 
 hbs.registerPartials(path.join(__dirname, '../views/partials'));
+
+hbs.registerHelper('selectedSport', (option, sport) => {
+    return option === sport ? 'selected' : '';
+})
+
+hbs.registerHelper('selectedDifficulty', (option, difficulty) => {
+    return option === difficulty ? 'selected' : '';
+})
--- controllers/routes.controller.js
+++ controllers/routes.controller.js
@@ -1,20 +1,20 @@
 const Route = require('../models/Route.model');
 
 module.exports.list = (req, res, next) => {
+    // const sport = Route.schema.path('sport').enumValues;
     const filters = req.query;
+    const { location, sport, difficulty } = req.query;
     const criterial = Object.keys(filters)
         .filter((key => filters[key] !== 'all'))
         .reduce((criterial, filter) => {
-            if (filters[filter]) {
-            criterial[filter] = filters[filter];
-            }
+            if (filters[filter]) criterial[filter] = filters[filter];
             return criterial;
         }, {});
 
         Route.find(criterial)
         .then(routes => {
-            console.log(criterial)
-            res.render('routes/list', { routes, filters })
+            console.log(sport);
+            res.render('routes/list', { routes, location, sport, difficulty })
         })
         .catch(next) 
 }
