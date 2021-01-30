3486633b847eb5cf82d7c770574a7925b16f6f81
asierbayon <bayonasier@gmail.com>
2021-01-30 18:42:13 +0100

    feat: dropdown selected and render standardized with a helper & global consts declared for sports and difficulties

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 hbs.registerPartials(path.join(__dirname, '../views/partials'));
 
-hbs.registerHelper('option', (option, value) => {
-    return option === value ? ' selected' : '';
-})
+hbs.registerHelper('option', function (selectedValue, value) {
+    const selectedProperty = value == selectedValue ? 'selected' : '';
+    return new hbs.SafeString(`<option value=${value} ${selectedProperty}>${value}</option>`);
+  });
--- controllers/routes.controller.js
+++ controllers/routes.controller.js
@@ -1,12 +1,11 @@
-const Route = require('../models/Route.model');
+const Route = require('../models/route.model');
+const constants = require('../public/js/constants');
 
 module.exports.list = (req, res, next) => {
-    // const sport = Route.schema.path('sport').enumValues;
     const filters = req.query;
-    const { key } = req.query;
     const { location, sport, difficulty } = req.query;
     const criterial = Object.keys(filters)
-        .filter((key => filters[key] !== 'all'))
+        .filter((key => filters[key] !== 'All'))
         .reduce((criterial, filter) => {
             if (filters[filter]) criterial[filter] = filters[filter];
             return criterial;
@@ -14,7 +13,14 @@ module.exports.list = (req, res, next) => {
 
         Route.find(criterial)
         .then(routes => {
-            res.render('routes/list', { routes, location, sport, difficulty })
+            res.render('routes/list', { 
+                routes, 
+                location, 
+                sport, 
+                difficulty, 
+                sportOptions: constants.SPORTS, 
+                difficultyOptions: constants.DIFFICULTIES
+            })
         })
         .catch(next) 
 }
