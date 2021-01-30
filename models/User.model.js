a48f88ae0c332c087f5127014a88d777be55bbc8
Toni Sanmartin <tonisanmartin@MacBook-Pro-de-Toni.local>
2021-01-30 09:34:11 +0100

    feat UserSchema 2

--- models/User.model.js
+++ models/User.model.js
@@ -3,13 +3,21 @@ const Schema = mongoose.Schema;
 
 const bcrypt = require('bcrypt');
 const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const URL_PATTERN = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
 
 const userSchema = new Schema({
-    name: {
+    username: {
       type: String,
       required: [true, 'Name is required'],
       minlength: [3, 'Name needs at last 3 chars'],
-      trim: true
+      unique: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: 'Full Name is required',
+      minlength: [3, 'Name needs at last 3 chars'],
+      trim: true,
     },
     email: {
       type: String,
@@ -25,13 +33,24 @@ const userSchema = new Schema({
       match: [PASSWORD_PATTERN, 'Password needs at least 8 chars'],
     },
     socialLogin: {
-        slack: String,
-        google: String
+      slack: String,
+      google: String,
     },
     avatar: {
-        type: String,
-        trim: true
-      },
+      type: String,
+      trim: true,
+    },
+    location: {
+      type: String,
+    },
+    website: {
+      type: String,
+      match: [URL_PATTERN, 'Invalid URL'],
+      trim: true,
+    },
+    bio: {
+      type: String,
+    },
   },
   { timestamps: true },
 );
