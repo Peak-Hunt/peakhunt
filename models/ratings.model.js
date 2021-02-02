bc2d7406ada0606f3b4818a19c63b02df703fd25
asierbayon <bayonasier@gmail.com>
2021-02-02 19:21:38 +0100

    feat: basic ratings model created

---
+++ models/ratings.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ratingSchema = new Schema({
+    description: {
+        type: String,
+        minlength: [12, 'Your description is not long enough.']
+    },
+    rating: {
+        type: number,
+        required: true
+    },
+    route: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Route'
+    }
+}, { timestamps: true });
+
+const rating = mongoose.model('Rating', ratingSchema);
+module.exports = Rating;
