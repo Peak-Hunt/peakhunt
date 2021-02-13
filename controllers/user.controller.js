5f0879621bc8852a78be16019072d0aca7da6910
Toni Sanmartin <tonisanmartin@MacBook-Pro-de-Toni.local>
2021-02-13 15:05:47 +0100

    feat email verification login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
-const passport = require('passport');
-const createError = require('http-errors');
-const mailer = require('../config/mailer.config');
-const User = require('../models/user.model');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const createError = require("http-errors");
+const mailer = require("../config/mailer.config");
+const User = require("../models/user.model");
+const flash = require("connect-flash");
 
 module.exports.register = (req, res, next) => {
   res.render("users/register");
@@ -17,35 +18,40 @@ module.exports.doRegister = (req, res, next) => {
   }
 
   User.findOne({ email: req.body.email })
-  .then((user) => {
-    if (user) {
-      renderWithErrors({ email: 'Email is already registered' });
-    } else {
-        return User.create(req.body)
-        .then(() => res.redirect("/"))
-    }
-  })
-  .catch((error) => {
-    if (error instanceof mongoose.Error.ValidationError) {
-        console.log(error)
-      renderWithErrors(error.errors);
-    } else {
-      next(error);
-    }
-  });
+    .then((user) => {
+      if (user) {
+        renderWithErrors({ email: "Email is already registered " });
+      } else {
+        return User.create(req.body).then((user) => {
+          mailer.sendValidationEmail(
+            user.email,
+            user.verified.token,
+            user.name
+          );
+          req.flash("data", JSON.stringify({ verification: true }));
+          res.redirect("/login");
+        });
+      }
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(error.errors);
+      } else {
+        next(error);
+      }
+    });
 };
 
 module.exports.login = (req, res, next) => {
   res.render("users/login");
 };
 
-
 module.exports.doLogin = (req, res, next) => {
   passport.authenticate("local-auth", (error, user, validations) => {
     if (error) {
       next(error);
     } else if (!user) {
-    res
+      res
         .status(400)
         .render("users/login", { user: req.body, errors: validations });
     } else {
@@ -58,22 +64,37 @@ module.exports.doLogin = (req, res, next) => {
 };
 
 module.exports.loginWithGoogle = (req, res, next) => {
-    passport.authenticate('google-auth', (error, user, validations) => {
-      if (error) {
-        next(error);
-      } else if (!user) {
-        res.status(400).render('users/login', { user: req.body, errors: validations });
-      } else {
-        req.login(user, error => {
-          if (error) next(error)
-          else res.redirect('/')
-        })
-      }
-    })(req, res, next);
-  }
+  passport.authenticate("google-auth", (error, user, validations) => {
+    if (error) {
+      next(error);
+    } else if (!user) {
+      res
+        .status(400)
+        .render("users/login", { user: req.body, errors: validations });
+    } else {
+      req.login(user, (error) => {
+        if (error) next(error);
+        else res.redirect("/");
+      });
+    }
+  })(req, res, next);
+};
 
-  module.exports.logout = (req, res, next) => {
-    req.logout();
-    res.redirect('/login');
-  };
-  
+module.exports.logout = (req, res, next) => {
+  req.logout();
+  res.redirect("/login");
+};
+
+module.exports.activate = (req, res, next) => {
+  User.findOneAndUpdate(
+    { 'verified.token': req.query.token },
+    { $set: { verified: { date: new Date(), token: null } } },
+    { runValidators: true }
+  ).then(user => {
+    if (!user) {
+      next(httpError(404, 'Invalid activation token or expired'))
+    } else {
+      res.redirect('/login');
+    }
+  }).catch(next);
+};
