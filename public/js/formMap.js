1a2a46f0607324c530b9d8ecc07e27b8e9714340
asierbayon <bayonasier@gmail.com>
2021-02-14 23:45:47 +0100

    feat: preserve location when editing route

--- public/js/formMap.js
+++ public/js/formMap.js
@@ -1,9 +1,10 @@
+const routeLocation = JSON.parse(document.getElementById('formMap').dataset.location);
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXNpZXJiYXlvbiIsImEiOiJja2w0NW0wOGwxNm9yMnFvNGJoa3hleWYyIn0.tStWnu8Y2c9X9cqyDhGVpw';
 
 var map = new mapboxgl.Map({
     container: 'formMap',
     style: 'mapbox://styles/asierbayon/ckl468ovn32wm17rm9h60p16s',
-    center: [8.551813819082557, 47.467729453922765], // [lng, lat]
+    center: routeLocation.coordinates, // [lng, lat]
     zoom: 11,
 });
 
@@ -16,7 +17,9 @@ var geocoder = new MapboxGeocoder({
 
 map.addControl(geocoder);
 
-geocoder.on('result', function(result) {
+geocoder.on('result', function (result) {
     document.getElementById('location').value = result.result.center;
     document.getElementById('locationAddress').value = result.result.place_name
- })
+})
+
+new mapboxgl.Marker().setLngLat(routeLocation.coordinates).addTo(map);
