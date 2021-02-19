7b761aae076766c9b5db25ee2bfebca01ed168da
asierbayon <bayonasier@gmail.com>
2021-02-19 23:31:27 +0100

    feat: pagination logic implemented

--- controllers/routes.controller.js
+++ controllers/routes.controller.js
@@ -3,19 +3,25 @@ const mongoose = require('mongoose');
 const createError = require('http-errors');
 const constants = require('../public/js/constants');
 
-module.exports.list = (req, res, next) => {
+module.exports.list = async (req, res, next) => {
     const { location, distanceFromLocation, locationAddress } = req.query;
     let lng, lat;
     if (location) [lng, lat] = location.split(',');
     const radius = distanceFromLocation / 6378.1; // Radius of the Earth
 
-    const filters = {...req.query};
+    const page = parseInt(req.query.page);
+    delete req.query.page;
+    const limit = 3;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const filters = { ...req.query };
     delete filters.distanceFromLocation;
     if (filters['elevationGained']) minAndMaxQuery('elevationGained');
     if (filters['duration']) minAndMaxQuery('duration');
     if (filters['distance']) minAndMaxQuery('distance');
 
-    function minAndMaxQuery (attribute) {
+    function minAndMaxQuery(attribute) {
         if (filters[attribute].length > 0) {
             filters[attribute] = {
                 $gte: filters[attribute][0] || 0,
@@ -23,7 +29,7 @@ module.exports.list = (req, res, next) => {
             }
         }
     }
-    
+
     const criterial = Object.keys(filters)
         .filter((key => filters[key] !== 'All' && filters[key] !== 'all'))
         .reduce((criterial, filter) => {
@@ -31,20 +37,37 @@ module.exports.list = (req, res, next) => {
             return criterial;
         }, {});
     
+    const pagination = {};
+    if (endIndex < await Route.countDocuments().exec()) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+    if (startIndex > 0) {
+        pagination.previous = {
+            page: page - 1,
+            limit
+        }
+    }
+
     if (locationAddress) delete criterial.locationAddress;
 
-    if (location && radius) criterial.location =  { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
-    const routes = Route.find(criterial)
-    .then(routes => {
-        res.render('routes/list', {
-            routes,
-            form: req.query,
-            sportOptions: constants.SPORT_OPTIONS,
-            difficultyOptions: constants.DIFFICULTY_OPTIONS,
-            withinOptions: constants.DISTANCES_WITHIN_OPTIONS,
+    if (location && radius) criterial.location = { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    console.log(criterial)
+    const routes = await Route.find(criterial).limit(limit).skip(startIndex).exec()
+        .then(routes => {
+            console.log(pagination)
+            res.render('routes/list', {
+                routes,
+                form: req.query,
+                pagination,
+                sportOptions: constants.SPORT_OPTIONS,
+                difficultyOptions: constants.DIFFICULTY_OPTIONS,
+                withinOptions: constants.DISTANCES_WITHIN_OPTIONS,
+            })
         })
-    })
-    .catch(next)
+        .catch(next)
 }
 
 module.exports.detail = (req, res, next) => {
@@ -61,7 +84,7 @@ module.exports.edit = (req, res, next) => {
 }
 
 module.exports.doEdit = (req, res, next) => {
-    req.body.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x)}
+    req.body.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x) }
     const route = Object.assign(req.route, req.body);
     route.save()
         .then(route => res.redirect(`/route/${route.id}`))
@@ -85,14 +108,14 @@ module.exports.create = (req, res, next) => {
 }
 
 module.exports.doCreate = (req, res, next) => {
-    
+
     const route = {
         ...req.body,
         user: req.user.id
-    } 
+    }
     console.log(route)
 
-    route.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x)}
+    route.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x) }
     Route.create(route)
         .then(() => res.redirect('/routes'))
         .catch(error => {
