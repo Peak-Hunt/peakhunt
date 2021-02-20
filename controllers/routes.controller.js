88922c705f0f3f213cdfedfd9cef613e11add404
asierbayon <bayonasier@gmail.com>
2021-02-20 18:20:43 +0100

    feat: doEdit route form only for route owner

--- controllers/routes.controller.js
+++ controllers/routes.controller.js
@@ -5,57 +5,57 @@ const constants = require('../public/js/constants');
 
 module.exports.list = async (req, res, next) => {
     try {
-    const { location, distanceFromLocation, locationAddress } = req.query;
-    let lng, lat;
-    if (location) [lng, lat] = location.split(',');
-    const radius = distanceFromLocation / 6378.1; // Radius of the Earth
+        const { location, distanceFromLocation, locationAddress } = req.query;
+        let lng, lat;
+        if (location) [lng, lat] = location.split(',');
+        const radius = distanceFromLocation / 6378.1; // Radius of the Earth
 
-    const page = parseInt(req.query.page) || 1;
-    delete req.query.page;
-    const limit = 9;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+        const page = parseInt(req.query.page) || 1;
+        delete req.query.page;
+        const limit = 9;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
 
-    const filters = { ...req.query };
-    delete filters.distanceFromLocation;
-    if (filters['elevationGained']) minAndMaxQuery('elevationGained');
-    if (filters['duration']) minAndMaxQuery('duration');
-    if (filters['distance']) minAndMaxQuery('distance');
+        const filters = { ...req.query };
+        delete filters.distanceFromLocation;
+        if (filters['elevationGained']) minAndMaxQuery('elevationGained');
+        if (filters['duration']) minAndMaxQuery('duration');
+        if (filters['distance']) minAndMaxQuery('distance');
 
-    function minAndMaxQuery(attribute) {
-        if (filters[attribute].length > 0) {
-            filters[attribute] = {
-                $gte: filters[attribute][0] || 0,
-                $lte: filters[attribute][1] || 1000000,
+        function minAndMaxQuery(attribute) {
+            if (filters[attribute].length > 0) {
+                filters[attribute] = {
+                    $gte: filters[attribute][0] || 0,
+                    $lte: filters[attribute][1] || 1000000,
+                }
             }
         }
-    }
 
-    const criterial = Object.keys(filters)
-        .filter((key => filters[key] !== 'All' && filters[key] !== 'all'))
-        .reduce((criterial, filter) => {
-            if (filters[filter]) criterial[filter] = filters[filter];
-            return criterial;
-        }, {});
-    
-    const pagination = {};
-    if (endIndex < await Route.countDocuments().exec()) {
-        pagination.next = {
-            page: page + 1,
-            limit
-        }
-    }
-    if (startIndex > 0) {
-        pagination.previous = {
-            page: page - 1,
-            limit
-        }
-    }
+        const criterial = Object.keys(filters)
+            .filter((key => filters[key] !== 'All' && filters[key] !== 'all'))
+            .reduce((criterial, filter) => {
+                if (filters[filter]) criterial[filter] = filters[filter];
+                return criterial;
+            }, {});
 
-    if (locationAddress) delete criterial.locationAddress;
-    if (location && radius) criterial.location = { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
-  
-    const routes = await Route.find(criterial).limit(limit).skip(startIndex).exec()
+        const pagination = {};
+        if (endIndex < await Route.countDocuments().exec()) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+        if (startIndex > 0) {
+            pagination.previous = {
+                page: page - 1,
+                limit
+            }
+        }
+
+        if (locationAddress) delete criterial.locationAddress;
+        if (location && radius) criterial.location = { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+
+        const routes = await Route.find(criterial).limit(limit).skip(startIndex).exec()
         if (routes) {
             res.render('routes/list', {
                 routes,
@@ -66,7 +66,7 @@ module.exports.list = async (req, res, next) => {
                 withinOptions: constants.DISTANCES_WITHIN_OPTIONS,
             })
         }
-    } catch(error) {
+    } catch (error) {
         next(error);
     }
 }
@@ -81,24 +81,26 @@ module.exports.edit = (req, res, next) => {
         const difficulties = constants.DIFFICULTIES;
         res.render('routes/edit', { route: req.route, sports, difficulties });
     } else res.redirect(`/route/${req.route.id}`);
-        
+
 }
 
 module.exports.doEdit = (req, res, next) => {
-    req.body.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x) }
-    const route = Object.assign(req.route, req.body);
-    route.save()
-        .then(route => res.redirect(`/route/${route.id}`))
-        .catch(error => {
-            if (error instanceof mongoose.Error.ValidationError) {
-                res.render('routes/edit', {
-                    route: req.route,
-                    sports: constants.SPORTS,
-                    difficulties: constants.DIFFICULTIES,
-                    errors: error.errors
-                });
-            } else next(error);
-        });
+    if (req.route.user.id === req.user.id) {
+        req.body.location = { type: 'Point', coordinates: (req.body.location).split(',').map(x => +x) }
+        const route = Object.assign(req.route, req.body);
+        route.save()
+            .then(route => res.redirect(`/route/${route.id}`))
+            .catch(error => {
+                if (error instanceof mongoose.Error.ValidationError) {
+                    res.render('routes/edit', {
+                        route: req.route,
+                        sports: constants.SPORTS,
+                        difficulties: constants.DIFFICULTIES,
+                        errors: error.errors
+                    });
+                } else next(error);
+            });
+    } else res.redirect(`/route/${req.route.id}`);
 }
 
 module.exports.create = (req, res, next) => {
@@ -139,7 +141,7 @@ module.exports.delete = (req, res, next) => {
 }
 
 module.exports.myRoutes = (req, res, next) => {
-    Route.find( { user: { $eq: req.user.id }})
+    Route.find({ user: { $eq: req.user.id } })
         .then(routes => res.render('routes/myRoutes', {
             routes
         }))
