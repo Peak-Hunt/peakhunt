6fec7082971d8a601ee973db2daa3d4a4f025387
asierbayon <bayonasier@gmail.com>
2021-02-20 17:29:06 +0100

    fix: seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 const Route = require("../models/routes.model");
 require('../config/db.config');
@@ -7,7 +9,17 @@ const routes = [
         title: 'Säntis via Lisengrat',
         sport: 'hiking',
         difficulty: 'Medium',
-        location: 'Appenzell, Switzerland',
+        location: {
+            "type": "Point",
+            "coordinates": [
+                9.4287025,
+                47.2856635
+            ]
+        },
+        locationAddress: "Wasserauen station, Weissbad, Appenzell Innerrhoden 9057, Switzerland",
+        distance: 10,
+        elevationGained: 1000,
+        elevationLost: 100,
         duration: 5,
         description: 'Interesting and exposed route through the eastern swiss alps.',
         image: 'https://static.az-cdn.ch/__ip/dd6Mbuh7EOJyH6SzgW4JEXrQY70/788615ca7370e56f2c91d2373bd8124e20b9459c/n-ch12-16x9-far'
@@ -16,7 +28,17 @@ const routes = [
         title: 'Matterhorn via Hörnli Ridge',
         sport: 'mountaineering',
         difficulty: 'Expert',
-        location: 'Zermatt, Switzerland',
+        location: {
+            "type": "Point",
+            "coordinates": [
+                9.4287025,
+                47.2856635
+            ]
+        },
+        locationAddress: "Wasserauen station, Weissbad, Appenzell Innerrhoden 9057, Switzerland",
+        distance: 10,
+        elevationGained: 1000,
+        elevationLost: 100,
         duration: 16,
         description: 'Climb the most famous mountain in Switzerland!.',
         image: 'https://images.unsplash.com/photo-1470104240373-bc1812eddc9f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
@@ -26,13 +48,25 @@ const routes = [
         title: 'Mont Blanc via Gouter',
         sport: 'mountaineering',
         difficulty: 'Expert',
-        location: 'Chamonix, Switzerland',
+        location: {
+            "type": "Point",
+            "coordinates": [
+                9.4287025,
+                47.2856635
+            ]
+        },
+        locationAddress: "Wasserauen station, Weissbad, Appenzell Innerrhoden 9057, Switzerland",
+        distance: 10,
+        elevationGained: 1000,
+        elevationLost: 100,
         duration: 16,
         description: 'Climb the highest mountain in Europe!.',
         image: 'https://images.unsplash.com/photo-1522527414937-8b7363e8701d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80',
         video: 'https://www.youtube.com/watch?v=WCLAHDCSdgE'
     },
 ];
+
+console.log('creating routes...')
 
 Route.create(routes)
     .then(routes => {
