c8e54842a612fbb9a94eef1e603b6e2650c1a703
asierbayon <bayonasier@gmail.com>
2021-02-22 00:55:58 +0100

    feat: avatar taken from google when social login

--- config/passport.config.js
+++ config/passport.config.js
@@ -10,72 +10,77 @@ passport.serializeUser((user, next) => {
 
 passport.deserializeUser((id, next) => {
     User.findById(id)
-      .then(user => next(null, user))
-      .catch(next);
+        .then(user => next(null, user))
+        .catch(next);
 });
 
 passport.use('local-auth', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
-  }, (email, password, next) => {
+}, (email, password, next) => {
     User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          next(null, null, { email: 'Invalid email or password'})
-        } else {
-          return user.checkPassword(password)
-            .then(match => {
-              if (match) {
-                if (user.verified && user.verified.date) {
-                  next(null, user)
-                } else {
-                  next(null, null, { email: 'Please check your email' })
-                }
-              } else {
+        .then(user => {
+            if (!user) {
                 next(null, null, { email: 'Invalid email or password' })
-              }
-            })
-        }
-      }).catch(next)
-  }));
-  
-  
-  passport.use('google-auth', new GoogleStrategy({
+            } else {
+                return user.checkPassword(password)
+                    .then(match => {
+                        if (match) {
+                            if (user.verified && user.verified.date) {
+                                next(null, user)
+                            } else {
+                                next(null, null, { email: 'Please check your email' })
+                            }
+                        } else {
+                            next(null, null, { email: 'Invalid email or password' })
+                        }
+                    })
+            }
+        }).catch(next)
+}));
+
+
+passport.use('google-auth', new GoogleStrategy({
     clientID: process.env.G_CLIENT_ID,
     clientSecret: process.env.G_CLIENT_SECRET,
     callbackURL: process.env.G_REDIRECT_URI || '/authenticate/google/cb',
-  }, (accessToken, refreshToken, profile, next) => {
+}, (accessToken, refreshToken, profile, next) => {
+    const providerData = profile._json;
     const googleId = profile.id;
-    const name = profile.displayName;
     const email = profile.emails[0] ? profile.emails[0].value : undefined;
+    const name = email.split('@')[0];
+    const avatar = (providerData.picture) ? providerData.picture : undefined;
 
     if (googleId && name && email) {
-        User.findOne({ $or: [
-            { email },
-            { 'social.google': googleId }
-        ]})
-        .then(user => {
-            if(!user) {
-                user = new User({
-                    name: email.split('@')[0],
-                    email,
-                    social: {
-                        google: googleId
-                    },
-                    verified: {
-                        date: new Date()
-                    },
-                    password: mongoose.Types.ObjectId()
-                });
-                return user.save()
-                    .then(user => next(null,user));
+        User.findOne({
+            $or: [
+                { email },
+                { 'social.google': googleId }
+            ]
+        })
+            .then(user => {
+                if (!user) {
+                    user = new User({
+                        avatar,
+                        name,
+                        email,
+                        social: {
+                            google: googleId
+                        },
+                        verified: {
+                            date: new Date()
+                        },
+                        password: mongoose.Types.ObjectId()
+                    });
+                    return user.save()
+                        .then(user => next(null, user));
                 } else {
-                    next(null,user);
+                    next(null, user);
                 }
-        }).catch(next)
+            }).catch(next)
 
     } else {
-        next(null, null, {auth: 'Invalid configuration'})
+        next(null, null, { auth: 'Invalid configuration' })
     }
-  
-  }));
+
+}));
