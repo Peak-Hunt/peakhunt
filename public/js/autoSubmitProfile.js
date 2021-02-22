0d6f363326e7a6def053164729520514d50fe33f
asierbayon <bayonasier@gmail.com>
2021-02-22 01:38:40 +0100

    feat: auto submit profile when image updated

---
+++ public/js/autoSubmitProfile.js
@@ -0,0 +1,6 @@
+document.getElementById('upload-avatar').addEventListener('change', function () {
+    this.form.submit();
+});
+document.getElementById('upload-banner').addEventListener('change', function () {
+    this.form.submit();
+});
