1013bcc52d40e2235a0b92b578a6e531d32b7b95
Toni Sanmartin <tonisanmartin@MacBook-Pro-de-Toni.local>
2021-02-22 21:40:29 +0100

    feat isnotauth

--- middlewares/secure.mid.js
+++ middlewares/secure.mid.js
@@ -5,3 +5,11 @@ module.exports.isAuthenticated = (req, res, next) => {
       res.status(401).redirect('/login');
     }
   };
+
+module.exports.isNotAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        res.status(401).redirect('/settings');
+    } else {
+        next();
+    }
+};
