57fa4a4b4963da88e8342cb54a08ffc8f2ef7139
asierbayon <bayonasier@gmail.com>
2021-02-23 09:44:51 +0100

    feat: register form for laptops

---
+++ public/js/showRegisterForm.js
@@ -0,0 +1,6 @@
+document.getElementById('showForm').onclick = function() {
+    document.getElementById('registerForm').style.display = 'block';
+    document.getElementById('showForm').style.display = 'none';
+    document.getElementById('or').style.display = 'none';
+    document.getElementById('google-btn').style.display = 'none';
+};
